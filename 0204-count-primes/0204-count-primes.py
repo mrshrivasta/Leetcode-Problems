bf6e5376cf2ef8b56e136ff918846256257a9d89
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-07 23:31:35 +0530

    Time: 399 ms (94.05%), Space: 97 MB (13.35%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2: return 0
+        sieve = [True] * n
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n**0.5)+1):
+            if sieve[i]:
+                sieve[i*i::i] = [False] * len(sieve[i*i::i])
+        return sum(sieve)
