4ce31f1d56aa195781b016e226d1f7a8ed6c9cdf
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-15 12:26:44 +0530

    Time: 157 ms (23.79%), Space: 90.8 MB (34.71%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,11 @@
+var flat = function(arr, n) {
+    const result = [];
+    for (const item of arr) {
+        if (Array.isArray(item) && n > 0) {
+            result.push(...flat(item, n - 1));
+        } else {
+            result.push(item);
+        }
+    }
+    return result;
+};
