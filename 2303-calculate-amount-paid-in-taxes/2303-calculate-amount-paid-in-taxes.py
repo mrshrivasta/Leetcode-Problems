9c3f51d146261c3f43d9cd729b78bace83394e97
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-22 13:10:06 +0530

    Time: 4 ms (10.76%), Space: 19.4 MB (66.49%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        tax, prev = 0, 0
+        for upper, percent in brackets:
+            tax += (min(income, upper) - prev) * percent / 100
+            prev = upper
+            if income <= upper:
+                break
+        return tax
