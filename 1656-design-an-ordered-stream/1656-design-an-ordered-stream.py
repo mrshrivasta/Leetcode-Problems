0a89a6dc11f4898540801b925ce2a84941bc30fd
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-22 13:16:50 +0530

    Time: 18 ms (40.82%), Space: 20.1 MB (8.61%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,12 @@
+class OrderedStream:
+    def __init__(self, n: int):
+        self.data = [None] * (n + 1)
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.data[idKey] = value
+        res = []
+        while self.ptr < len(self.data) and self.data[self.ptr]:
+            res.append(self.data[self.ptr])
+            self.ptr += 1
+        return res
