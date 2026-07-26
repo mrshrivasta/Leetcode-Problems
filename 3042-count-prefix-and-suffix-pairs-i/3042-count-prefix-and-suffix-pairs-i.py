67c2182ff71ee508794d41371bab578d670b680b
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-26 18:56:55 +0530

    Time: 7 ms (67.41%), Space: 19.4 MB (4.27%) - LeetHub

---
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -0,0 +1,4 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: list[str]) -> int:
+        return sum(words[j].startswith(words[i]) and words[j].endswith(words[i])
+                   for i in range(len(words)) for j in range(i+1, len(words)))
