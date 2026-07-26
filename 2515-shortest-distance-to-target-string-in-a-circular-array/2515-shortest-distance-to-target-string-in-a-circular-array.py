948bbff5000998edee3736061dd6205545cb27d1
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-26 19:01:01 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.13%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -0,0 +1,4 @@
+class Solution:
+    def closestTarget(self, words: list[str], target: str, startIndex: int) -> int:
+        n = len(words)
+        return min((min(abs(i - startIndex), n - abs(i - startIndex)) for i, w in enumerate(words) if w == target), default=-1)
