a058903c55c9efe19aaf13cef506b15c0c597fd4
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-26 18:48:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (15.88%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        digit_len = 1
+        count = 9
+        start = 1
+
+        while n > digit_len * count:
+            n -= digit_len * count
+            digit_len += 1
+            count *= 10
+            start *= 10
+
+        num = start + (n - 1) // digit_len
+        return int(str(num)[(n - 1) % digit_len])
