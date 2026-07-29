3151889a8e0a41b129571cfd51cbfc94774eb5e9
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-29 09:34:14 +0530

    Time: 0 ms (100%), Space: 19.5 MB (11.07%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        special = set("!@#$%^&*()-+")
+        return (len(password) >= 8 and
+                any(c.islower() for c in password) and
+                any(c.isupper() for c in password) and
+                any(c.isdigit() for c in password) and
+                any(c in special for c in password) and
+                all(password[i] != password[i+1] for i in range(len(password)-1)))
