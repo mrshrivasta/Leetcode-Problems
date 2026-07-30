eb19eff794e81ed1707f12369476759b90219790
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-30 10:26:16 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.34%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        p_to_w = {}
+        w_to_p = {}
+        for p, w in zip(pattern, words):
+            if p_to_w.get(p, w) != w or w_to_p.get(w, p) != p:
+                return False
+            p_to_w[p] = w
+            w_to_p[w] = p
+        return True
